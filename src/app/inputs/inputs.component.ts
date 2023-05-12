1c67f460ab056574e93291d94162c67ef8e077b8
nicolasmiguezs <127797694+nicolasmiguezs@users.noreply.github.com>
2023-05-12 20:05:30 -0300

    button function and save the inputs variables. showing consol while define ul and li

--- src/app/inputs/inputs.component.ts
+++ src/app/inputs/inputs.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./inputs.component.css']
 })
 export class InputsComponent {
+  
 
+  addTask() {
+    console.log("Agregar tarea")
+    const inputTitle = document.getElementById('input-ul') as HTMLInputElement | null;
+
+    const taskTitle = inputTitle?.value;
+    console.log(taskTitle)
+    
+
+    const inputDesc = document.getElementById('desc-ul') as HTMLInputElement | null;
+
+    const taskDesc = inputDesc?.value;
+    console.log(taskDesc)
+
+  }
+
+  async getCats() {
+    const response = await fetch("https://catfact.ninja/fact");
+    const jsonData = await response.json();
+    return jsonData;
+  }
+
+  
+
+  seeCat(cat: { fact: any; }){
+    console.log(cat.fact)
 }
+
+  addCatsk(){
+    let json=this.getCats();
+    console.log(json.then(this.seeCat))
+  }
+
+  addLink()
+  {
+    window.open("https://www.youtube.com/watch?v=jIQ6UV2onyI&t=32s")
+  }
+}
+
+
