075f38970967ee4090a1db4e1d454883e931e3b2
RODRIGO LUQUE <rodrigoluquepuig@gmail.com>
2023-06-07 13:21:52 -0300

    refactor for api

--- src/app/card-list/card-list.component.ts
+++ src/app/card-list/card-list.component.ts
@@ -1,19 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Task } from '../interfaces/task.interface';
 import { ApiService } from '../services/api.service';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './card-list.component.html',
   styleUrls: ['./card-list.component.css']
 })
-export class CardListComponent {
+export class CardListComponent implements OnInit {
+  
+  controlInputTitle: FormControl = new FormControl('', { validators: [Validators.required] })
+  controlInputDescription: FormControl = new FormControl('', { validators: [Validators.required] })
   tasks: Task[] = [];
 
-  constructor(private apiService: ApiService ){
-    this.apiService.getAllTasks()
-    .then(tasks=> {
-      this.tasks = tasks
+  constructor(private apiService: ApiService ){ }
+
+  ngOnInit(): void {
+    this.apiService.getAllTasksFromUser('1')
+    .subscribe({
+      next: tasks => this.tasks = tasks
+    })
+  }
+  
+  addTask() {
+    if (this.controlInputTitle.invalid || this.controlInputDescription.invalid) {
+      return;
+    }
+
+    const newTask: Task = {
+      title: this.controlInputTitle.value,
+      description: this.controlInputDescription.value,
+      done: false
+    }
+    this.apiService.createTask("1", newTask).subscribe({
+      next: () => this.tasks.push(newTask),
+    })
+  }
+
+  async addCatsk() {
+    let catFact: string = await this.apiService.getCatTip()
+    const newTask: Task = {
+      title: "Random Cat Fact",
+      description: catFact,
+      done: false
+    }
+    this.apiService.createTask("1", newTask).subscribe({
+      next: () => this.tasks.push(newTask)
+    })
+  }
+
+  routeToVideo() {
+    window.open("https://www.youtube.com/watch?v=jIQ6UV2onyI&t=32s")
+  }
+
+  removeTask(task: Task) {
+    this.apiService.deleteTaskById("1", task.id!.toString()).subscribe({
+      next: () => this.tasks = this.tasks.filter(t => t.id !== task.id)
     })
   }
 }
---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,9 @@
+import { Task } from "./task.interface";
+
+export interface User {
+    id?: string;
+    name: string;
+    email: string;
+    password: string;
+    tasks?: Task[];
+}
